6fdf599bda44ef268bf5bc1d2fa6dc8536775fa8
Salem-1 <61345528+Salem-1@users.noreply.github.com>
2022-07-18 21:02:48 +0400

    Update get_next_line.c
    the backup of the working version

--- get_next_line.c
+++ get_next_line.c
@@ -6,45 +6,96 @@
 /*   By: ahsalem <ahsalem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 12:13:00 by ahsalem           #+#    #+#             */
-/*   Updated: 2022/06/22 06:02:37 by ahsalem          ###   ########.fr       */
+/*   Updated: 2022/06/29 07:05:12 by ahsalem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-//change the program to save the remainging on top of the tmp as follows
-//tmp + \0 + remainignig
-//this is a static variable to be returned
-//all other operations do on temporary strings and free them before the return statement
+//the processing of the returned reader before reading again
+//clean it before the retuning the reader itself to test and process, 
+//if found null return the error, an this will solve end of file error
 
+//solve the strjoin error 
 
-
-//start your program with the remaining str instead of tmp, check it, 
-//if !(\n || \0) call the read and do your job 
 char *get_next_line(int fd)
 {
+	static char	reader[BUFFER_SIZE + 1];
 	char		*result;
-	char		*tmp;
-	int			counter;
-
-	counter = 0;
-	tmp =  NULL;
-	result = NULL;
-	result = malloc(BUFFER_SIZE);
-	if (!result)
-		return(NULL);
-	//read(fd, result, BUFFER_SIZE);
-	//if (result[0] == -1 || !*result)
-		//return (check_err(result, "there is nothing else to read\n"));
-	tmp = filler(fd, result, tmp);
+	int			read_len;
 	
-	printf("\nbefore ending the program, tmp is <%s>", tmp);
-	return (tmp);
+	result = NULL;
+	// printf("begining reader = <%s>\n", reader);
+	if (reader[0])
+	{
+		result = ft_mystrcpy(result, reader);
+		read_len = check_null_or_nl(result, ft_strlen(result) + 1);
+		if (result[read_len - 1] == '\0')
+		{
+			// printf("read_len = %d\n", read_len);
+			if (read_len == 0)
+				return (error_message("error: Nothing more to read\n")); 
+			read_len = read(fd, reader, BUFFER_SIZE);
+			reader[read_len] = '\0';
+			result = ft_strjoin(result, reader, ft_strlen(result) + read_len );	
+		}
+		result = filler(reader,result, fd);
+		return (result);
+	}
+	else
+	{
+		if (fd == -1)
+			return (error_message("error: Nothing more to read\n"));
+		read_len = read(fd, reader, BUFFER_SIZE);
+		if (read_len == 0)
+			return (error_message("error: Nothing more to read\n"));
+		reader[read_len] = '\0';
+		result = ft_mystrcpy(result, reader);
+		result = filler(reader,result, fd);
+		return (result);
+	}
+}
+		
+// char *clean_result(char * result, char *tmp, char *reader ,int counter)
+// {
+// 	int		new_line_index;
+// 	int		reader_index;
+// 	int		i;
+
+// 	i = -1;
+// 	new_line_index = check_null_or_nl(tmp, counter);
+// 	reader_index = check_null_or_nl(tmp, BUFFER_SIZE);
+// 	if (new_line_index == 0)
+// 	{
+// 		result = ft_strjoin(result, tmp, counter , 'c');
+// 		return (result);
+// 	}
+	
+// 	else if (new_line_index == -1)
+// 		return (error_message("error: Nothing more to read\n"));
+// 	result = ft_strjoin(result, tmp, new_line_index + 1, 'c');
+// 	result[new_line_index ] = '\0';
+// 	while (++reader_index < ft_strlen(reader))
+// 			reader[++i] = reader[reader_index];
+// 	return (result);
+// }
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = -1;
+	if(!str)
+		return (0);
+	while (str[++i])
+		;
+	return (i);
 }
 
-//fill the remaining memory with 0 ir \0 :)
-// char *memory_cleaner(char *tmp, static char *remaining)
-// {
-// 	//takes tmp, measure the len till \n or \0, clean the remainig at the null termination or the end of it 
-// 	//copy it to new str with exat size 
-// 	//free(tmp) & result &everything else needed.
-// }
+char *error_message(char *message)
+{
+	char *error;
+
+	error = NULL;
+	error = ft_mystrcpy(error, message);
+	return (error);
+}
